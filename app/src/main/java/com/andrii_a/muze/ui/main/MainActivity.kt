b0672be52704e921390f81128f22c208354d5af7
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-06-19 22:31:20 +0200

    Configured splash screen

--- app/src/main/java/com/andrii_a/muze/ui/main/MainActivity.kt
+++ app/src/main/java/com/andrii_a/muze/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andrii_a.muze.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
