0864971cd9b9b4c723d9a55acdb5ed79b72b6b11
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-06-19 22:30:51 +0200

    Enabled edge to edge app mode

--- app/src/main/java/com/andrii_a/muze/ui/main/MainActivity.kt
+++ app/src/main/java/com/andrii_a/muze/ui/main/MainActivity.kt
@@ -3,12 +3,16 @@ package com.andrii_a.muze.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             Muze()
         }
