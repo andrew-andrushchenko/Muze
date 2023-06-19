deb0c2f8fcb953f9b6f12f828c029940ede32e3e
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-06-19 21:45:08 +0200

    Added data transfer object models to retrieve data from server side

---
+++ app/src/main/java/com/andrii_a/muze/data/dto/ArtistDto.kt
@@ -0,0 +1,14 @@
+package com.andrii_a.muze.data.dto
+
+import com.andrii_a.muze.domain.models.Artist
+
+data class ArtistDto(
+    val id: Int,
+    val name: String,
+    val bornDateString: String?,
+    val diedDateString: String?,
+    val portraitImageUrl: String,
+    val bio: String
+) {
+    fun toArtist(): Artist = Artist(id, name, bornDateString, diedDateString, portraitImageUrl, bio)
+}
---
+++ app/src/main/java/com/andrii_a/muze/data/dto/ArtworkDto.kt
@@ -0,0 +1,16 @@
+package com.andrii_a.muze.data.dto
+
+import com.andrii_a.muze.domain.models.Artist
+import com.andrii_a.muze.domain.models.Artwork
+
+data class ArtworkDto(
+    val id: Int,
+    val name: String,
+    val year: String?,
+    val location: String,
+    val imageUrl: String,
+    val description: String?,
+    val artist: Artist
+) {
+    fun toArtwork(): Artwork = Artwork(id, name, year, location, imageUrl, description, artist)
+}
