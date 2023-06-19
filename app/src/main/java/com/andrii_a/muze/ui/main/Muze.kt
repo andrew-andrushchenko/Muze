8db0b01bedef77c3eacdb2a71835eca50d28ab1c
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-06-19 22:24:35 +0200

    Implemented navigation bar, components connected

---
+++ app/src/main/java/com/andrii_a/muze/ui/main/Muze.kt
@@ -0,0 +1,75 @@
+package com.andrii_a.muze.ui.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.rememberNavController
+import com.andrii_a.muze.ui.navigation.MainNavigationHost
+import com.andrii_a.muze.ui.navigation.NavigationScreen
+import com.andrii_a.muze.ui.navigation.NavigationScreenRoutes
+import com.andrii_a.muze.ui.theme.MuzeTheme
+import com.andrii_a.muze.ui.util.currentRoute
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun Muze() {
+    MuzeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            val systemUiController = rememberSystemUiController()
+            val navController = rememberNavController()
+
+            Box(modifier = Modifier.imePadding()) {
+                MainNavigationHost(
+                    navHostController = navController,
+                    systemUiController = systemUiController
+                )
+
+                if (navController.currentRoute in NavigationScreenRoutes) {
+                    NavigationBar(
+                        modifier = Modifier.align(Alignment.BottomCenter)
+                    ) {
+                        NavigationScreen.values().forEach { item ->
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = if (navController.currentRoute == item.route)
+                                            item.iconSelected
+                                        else
+                                            item.iconUnselected,
+                                        contentDescription = null
+                                    )
+                                },
+                                label = { Text(text = stringResource(id = item.titleRes)) },
+                                selected = navController.currentRoute == item.route,
+                                onClick = {
+                                    navController.navigate(item.route) {
+                                        launchSingleTop = true
+                                        restoreState = true
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
